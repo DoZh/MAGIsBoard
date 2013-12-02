0b0e10d1240d4912d1d0a238ca040e5788a4ecf1
MAGI <TATQAQTAT@gmail.com>
2013-12-02 14:54:41 +0800

    added RTC Module(DS3231)Programmer(USBASP) FirmWare
    added RTC Module(DS3231)Programmer(USBASP) FirmWare

---
+++ MAGI'sUSBASP/usbasp.2011-05-28.YUKI(2012-2-19)Autospeed/firmware/default/dep/main.o.d
@@ -0,0 +1,62 @@
+main.o: ../main.c c:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/inttypes.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/stdint.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/iom8.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/common.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/version.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/interrupt.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/pgmspace.h \
+  c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/wdt.h ../usbasp.h \
+  D:\!MCU\!YUKI_AVR\usbasp.2011-05-28_YUKI\firmware\usbdrv/usbdrv.h \
+  D:\!MCU\!YUKI_AVR\usbasp.2011-05-28_YUKI\firmware\./usbconfig.h \
+  D:\!MCU\!YUKI_AVR\usbasp.2011-05-28_YUKI\firmware\usbdrv/usbportability.h \
+  ../isp.h ../clock.h ../tpi.h ../tpi_defs.h
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/inttypes.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/stdint.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/iom8.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/common.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/version.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/interrupt.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/pgmspace.h:
+
+c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/wdt.h:
+
+../usbasp.h:
+
+D:\!MCU\!YUKI_AVR\usbasp.2011-05-28_YUKI\firmware\usbdrv/usbdrv.h:
+
+D:\!MCU\!YUKI_AVR\usbasp.2011-05-28_YUKI\firmware\./usbconfig.h:
+
+D:\!MCU\!YUKI_AVR\usbasp.2011-05-28_YUKI\firmware\usbdrv/usbportability.h:
+
+../isp.h:
+
+../clock.h:
+
+../tpi.h:
+
+../tpi_defs.h:
